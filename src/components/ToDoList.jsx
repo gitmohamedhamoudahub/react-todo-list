79bad72bed6c594b2a9a11914adaa09b0ee20f51
gitmohamedhamoudahub <eng.mohamed.hobby@gmail.com>
2025-02-13 15:59:07 -0500

    Showing data on screen

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,14 +1,32 @@
 import ToDoListItem from "./ToDoListItem.jsx";
+import {GetToDoListData} from "../data/ToDoListData.js"
+// import { useReducer } from "react";
 function ToDoList()
 {
+   
+    const ToDoListData = GetToDoListData();
+    // const [toDo, dispatch] = useReducer(reducer, [...ToDoListData]);
+    console.log("ToDoList", ToDoListData);
     return(
         <>
-        <div className="toDoListContainer">
-        <div className="toDoListHeader">To Do List</div>
         
-            <ToDoListItem/>
-            <ToDoListItem/>
-            <ToDoListItem/>       
+        <div className="toDoListContainer">
+        
+        <div className="toDoListHeader">To Do List <hr></hr></div>
+        
+        <div className="txtAddContainer">
+        <input type="text" placeholder="Add new task..." />
+        <button>📋 Add </button>
+        <hr></hr>
+        </div>
+        {ToDoListData.map((item, index) => (
+                    
+                    <ToDoListItem 
+                    key={index} 
+                    title={item.title} 
+                    completed={item.completed} 
+                    />
+                ))}
         </div>
         </>
     )
--- src/components/ToDoListItem.jsx
+++ src/components/ToDoListItem.jsx
@@ -1,11 +1,61 @@
-function ToDoListItem()
+import {useState, useReducer } from "react";
+
+function ToDoListItem({title,completed})
 {
+
+    const [toDo, dispatch] = useReducer(reducer, {title,completed});
+    const [DeleteEnabled, setDeleteEnabled] = useState(completed);
+    
+     function reducer(state, action)
+     {
+        const ACTIONS = {
+            ADD: 'add',
+            DELETE: 'delete',
+            EDIT: 'edit',
+            SELECT: 'select'
+            }
+
+         switch(action.type)
+         {
+             case "SELECT":
+                 {
+                    console.log(DeleteEnabled);
+                    setDeleteEnabled((flag)=> flag = !flag);
+                    console.log(DeleteEnabled);
+                    return {...state, completed:!state.completed}};
+             case "EDIT":
+                 return {...state, title: action.title};
+             case "ADD":
+                 return {...state, title: action.title};
+             case "DELETE":
+                 return {...state, title: action.title};
+                default:
+                 return state;
+         }
+     }
+    function handleEdit()
+    {
+        dispatch({type: "EDIT", title: "New Title"})
+    }     
+    function handleSelect()
+    {
+        dispatch({type: "SELECT"})
+    }
+    function handleCompleted()
+    {
+        setDeleteEnabled((prev)=> !prev)
+    }
+    console.log('completed = >',completed);
+    console.log('DeleteEnabled = >',DeleteEnabled);
     return(
     <>
     <div className="toDoListItem">
-        <input type="checkbox" />
-        <span>Task 1</span>
-        <button className="btnToDoDelete">🗑️ Delete</button>
+        <div><input type="checkbox" onChange={handleCompleted} checked={DeleteEnabled}/></div>
+    
+        <div><input className="txtTask" value={title} readOnly ></input></div>
+        <div><button className="btnToDoEdit" onClick={handleEdit}>📝 Edit</button></div>
+        <div><button className="btnToDoDelete" disabled={!DeleteEnabled}>🗑️ Delete</button></div>
+        
     </div>
     </>
     )
